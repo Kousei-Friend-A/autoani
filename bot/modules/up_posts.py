12a76db1db4fc5508711198f8747ea7371d3c7bd
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-20 07:33:44 +0530

    Update up_posts.py

--- bot/modules/up_posts.py
+++ bot/modules/up_posts.py
@@ -24,32 +24,6 @@ async def upcoming_animes():
         except Exception as err:
             await rep.report(str(err), "error")
     if not ffQueue.empty():
-        await ffQueue.join()from json import loads as jloads
-from os import path as ospath, execl
-from sys import executable
-
-from aiohttp import ClientSession
-from bot import Var, bot, ffQueue
-from bot.core.text_utils import TextEditor
-from bot.core.reporter import rep
-
-async def upcoming_animes():
-    if Var.SEND_SCHEDULE:
-        try:
-            async with ClientSession() as ses:
-                res = await ses.get("https://subsplease.org/api/?f=schedule&h=true&tz=Asia/Kolkata")
-                aniContent = jloads(await res.text())["schedule"]
-            text = "<b>📆 Today's Anime Releases Schedule</b>\n\n"
-            for i in aniContent:
-                aname = TextEditor(i["title"])
-                await aname.load_anilist()
-                text += f''' <a href="https://subsplease.org/shows/{i['page']}">{aname.adata.get('title', {}).get('english') or i['title']}</a>\n    • <b>Time</b> : {i["time"]} hrs\n\n'''
-                sch_list = text + "<b>⏰ Current TimeZone :</b> <code>IST (UTC +5:30)</code>"
-            TD_SCHR = await bot.send_message(Var.MAIN_CHANNEL, sch_list)
-            await (await TD_SCHR.pin()).delete()
-        except Exception as err:
-            await rep.report(str(err), "error")
-    if not ffQueue.empty():
         await ffQueue.join()
     await rep.report("Auto Restarting..!!", "info")
     execl(executable, executable, "-m", "bot")
