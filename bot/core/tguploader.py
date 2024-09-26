965cf01c50c18636c25b5d17ede7f89204f7ea0e
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-26 22:26:22 +0530

    Update tguploader.py

--- bot/core/tguploader.py
+++ bot/core/tguploader.py
@@ -58,9 +58,9 @@ class TgUploader:
             speed = current / diff 
             eta = round((total - current) / speed)
             bar = floor(percent/8)*"■" + (12 - floor(percent/8))*"□"
-            progress_str = f"""📌 <b>Anime Name :</b> <b><i>{self.__name}</i></b>
+            progress_str = f"""➤ <b>Anime Name :</b> <b><i>{self.__name}</i></b>
 
-● <b>Status :</b> <i>Uploading 📤</i>
+● <b>Status :</b> <i>Uploading..</i>
     <code>[{bar}]</code> {percent}%
     
     ● <b>Size :</b> {convertBytes(current)} out of ~ {convertBytes(total)}
