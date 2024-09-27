58362cc5c5a29b7dd3ddd881e1b0aac1a7edbb23
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-27 21:28:41 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -48,9 +48,9 @@ class Var:
     ADMINS = list(map(int, getenv("ADMINS", "1242011540").split()))
     
     SEND_SCHEDULE = "False"
-    FFCODE_1080 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 1920x1080 -pix_fmt yuv420p -crf 25 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
-    FFCODE_720 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 1280x720 -pix_fmt yuv420p -crf 25 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
-    FFCODE_480 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 640x480 -pix_fmt yuv420p -crf 25 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
+    FFCODE_1080 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 1920x1080 -pix_fmt yuv420p -crf 25 -c:a libopus -b:a 48k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
+    FFCODE_720 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 1280x720 -pix_fmt yuv420p -crf 25 -c:a libopus -b:a 48k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
+    FFCODE_480 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 640x480 -pix_fmt yuv420p -crf 25 -c:a libopus -b:a 48k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
     FFCODE_360 = """ffmpeg -i '{}' -progress '{}' -preset superfast -c:v libx264 -s 640x360 -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 '{}' -y"""
     QUALS = getenv("QUALS", "360 480 720 1080").split()
     
