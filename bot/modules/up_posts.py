0aad92444c077021a01b253cb7ae2516404e25fe
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-29 06:59:15 +0530

    Update up_posts.py

--- bot/modules/up_posts.py
+++ bot/modules/up_posts.py
@@ -39,7 +39,7 @@ async def send_schedule():
                 title = aname.adata.get('title', {}).get('english') or i['title']
                 time = i["time"]
                 aired_icon = "✅" if i["aired"] else ""
-                sch_list += f"[<code>{time}</code>] - 📌 <b>{title}</b> {aired_icon}\n"
+                sch_list += f"[<code>{time}</code>] - 📌 <b>{title}</b> {aired_icon}\n\n"
 
             text = f"<b>📆 Today's Anime Releases Schedule</b>\n\n{sch_list}<b>⏰ Current TimeZone :</b> <code>IST (UTC +5:30)</code>"
 
