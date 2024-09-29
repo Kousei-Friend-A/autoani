bb0a72cb0694af3b7031ed4864dfef53194f2638
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-29 10:38:15 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -53,7 +53,7 @@ async def queue_loop():
         await asleep(10)
 
 async def main():
-    sch.add_job(fetch_and_format_schedule, "cron", hour=10, minute=30)
+    sch.add_job(fetch_and_format_schedule, "cron", hour=10, minute=40)
     await bot.start()
     await restart()
     LOGS.info('Auto Anime Bot Started!')
