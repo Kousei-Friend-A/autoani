f0904215aaff7bf155374d79df1551af10402541
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-10-06 18:08:29 +0530

    Update auto_animes.py

--- bot/core/auto_animes.py
+++ bot/core/auto_animes.py
@@ -6,6 +6,7 @@ from aiofiles.os import remove as aioremove
 from traceback import format_exc
 from base64 import urlsafe_b64encode
 from time import time
+from pyrogram import utils
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot import bot, bot_loop, Var, ani_cache, ffQueue, ffLock, ff_queued
