71dcf8c188d5638609012340d58831389d16f25b
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-10-06 18:03:59 +0530

    Update func_utils.py

--- bot/core/func_utils.py
+++ bot/core/func_utils.py
@@ -23,6 +23,17 @@ from pyrogram.errors import MessageNotModified, FloodWait, UserNotParticipant, R
 from bot import bot, bot_loop, LOGS, Var
 from .reporter import rep
 
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
+
+utils.get_peer_type = get_peer_type_new
+
 def handle_logs(func):
     @wraps(func)
     async def wrapper(*args, **kwargs):
