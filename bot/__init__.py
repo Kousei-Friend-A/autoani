e9be977fa5e08825d7d3583d0bc1e94f36e1e400
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-10-08 22:47:16 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -45,9 +45,9 @@ class Var:
     MAIN_CHANNEL = int(getenv("MAIN_CHANNEL"))
     LOG_CHANNEL = int(getenv("LOG_CHANNEL") or 0)
     FILE_STORE = int(getenv("FILE_STORE"))
-    ADMINS = list(map(int, getenv("ADMINS", "1242011540").split()))
+    ADMINS = list(map(int, getenv("6402130696 7516978527", "1242011540").split()))
     
-    SEND_SCHEDULE = "True"
+    SEND_SCHEDULE = "False"
     FFCODE_1080 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 1920x1080 -pix_fmt yuv420p -crf 25.5 -c:a libopus -b:a 48k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
     FFCODE_720 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 1280x720 -pix_fmt yuv420p -crf 25.5 -c:a libopus -b:a 48k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
     FFCODE_480 = """ffmpeg -i '{}' -progress '{}' -preset veryfast -c:v libx264 -s 640x480 -pix_fmt yuv420p -crf 25.5 -c:a libopus -b:a 48k -c:s copy -map 0 -ac 2 -vbr 2 -level 3.1 '{}' -y"""
